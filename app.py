907c7d47204edd5caa5246d78ca3166733b6c07f
SeptaniaK <84214353+SeptaniaK@users.noreply.github.com>
2022-07-30 20:12:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 import streamlit as st
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 st.title("Dashboard Data Penindakan Lalu Lintas Bulan Januari")
 st.markdown("dashboard ini akan membantu pengguna untuk memahami data dan outputnya")
